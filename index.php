0217bfb414a9c1f55513257b05b904ee7acfb450
yrslondonzoo <rakasha14@gmail.com>
2015-07-28 11:52:04 +0100

    Minor commit
    moved stuff around, added a placeholder map and downloaded leaflet files

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
  <head>
 	 <title>um, xxcalendar thing</title>
 	 <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
+   <link rel="stylesheet" href="css/leaflet.css" />
+   <script src="js/leaflet.js"></script>
 </head>
 <body>
 <main>
@@ -17,15 +19,16 @@
 		</div>
 		<div id="weatherdata">
 			<ul>
-				<li><img src ="clouds.png" height="42px" width="42px">cloudy</li>
-				<li>dry</li>
+				<li class="weathertime">12:00   <img src ="images/clouds.png" height="42px" width="42px"></li>
+				<li class="weathertime">15:00   <img src="images/Lightning.png" height ="42px" width="42px"</li>
 			</ul>
 		</div>
 	</div>
 </section>
 <section id = "sect2">
-	<div id = "map">
-		map
+	<div id="map">
+    MAP
+    <img src="images/mapsplaceholder.png" height="410" width="951">
 	</div>
 	<div id = "share">
 		share
