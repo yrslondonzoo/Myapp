c8939eb981d27e70af653efd926820e85635740a
yrslondonzoo <rakasha14@gmail.com>
2015-07-28 10:12:36 +0100

    Initial Commit
    First commit

---
+++ index.php
@@ -0,0 +1,35 @@
+ <!DOCTYPE HTML>
+ <head>
+	 <title>um, xxcalendar thing</title>
+	 <link rel="stylesheet" href="/css/master.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+<main>
+<section id = "sect1">
+	<div id = "info">
+		<span id="date">1st</span>
+		<span id="title">Brad's Great Meetup</span>
+		<span id="location">London Zoo</span>
+	</div>
+	<div id = "weather">
+		<div id="weatherinfo">
+			weather
+		</div>
+		<div id="weatherdata">
+			<ul>
+				<li><img src ="clouds.png" height="42px" width="42px">cloudy</li>
+				<li>dry</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<section id = "sect2">
+	<div id = "map">
+		map
+	</div>
+	<div id = "share">
+		share
+	</div>
+</section>
+</main>
+</body>
